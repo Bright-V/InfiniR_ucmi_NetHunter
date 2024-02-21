021b66cffd18d63bc7b67252d5038ba16642714b
Yttehs <Yttehs-HDX@github.com>
2024-02-21 22:36:56 +0800

    Fetch upstream.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 # This script is used to build android kernel for mi10 #
 # ===----------------------------------------------=== #
 
-KERNEL_VERSION="2.3"
+KERNEL_VERSION="2.4"
 DEFCONFIG="ucmi_nethunter_apatch_defconfig"
 
 # ===------------=== #
